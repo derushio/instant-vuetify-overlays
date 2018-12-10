abddd82bacf2d9381b72341faaff6bce9c2ea079
derushio <narumi18wa@gmail.com>
2018-12-11 00:43:37 +0900

    <master>[modified] export components

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,6 @@
+import { VSnackbarComponent } from './components/vsnackbar';
+import { VDialogComponent } from './components/vdialog';
 declare const _default: any;
 export default _default;
+export { VSnackbarComponent, VDialogComponent };
 import '../src/types'
